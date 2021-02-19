2ff57c35845ded121beafb0d9904d81532c5b2c4
Ross Cowie <rossfcowie@gmail.com>
2021-02-19 15:39:13 +0000

    Unit testing for the action enumeration.

---
+++ src/test/java/com/qa/ims/controllers/ActionTest.java
@@ -0,0 +1,35 @@
+package com.qa.ims.controllers;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.qa.ims.controller.Action;
+import com.qa.ims.utils.Utils;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ActionTest {
+
+	@Mock
+	private Utils utils;
+	
+	private Action action;
+	
+	@Test
+	public void testGetAction() {
+		Mockito.when(utils.getString()).thenReturn("Create");
+		action = Action.getAction(utils);
+		assertEquals(Action.CREATE,action);
+		Mockito.verify(utils, Mockito.times(1)).getString();
+	}
+	@Test
+	public void testgetDescription() {
+		action = Action.CREATE;
+		assertEquals("CREATE: To save a new entity into the database" ,action.getDescription());
+	}
+}
