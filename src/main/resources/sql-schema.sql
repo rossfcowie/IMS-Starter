6129ca3dd3a9981769cce7f494b7529f20b1e9e0
Ross Cowie <rossfcowie@gmail.com>
2021-02-23 12:11:26 +0000

    IM-80,IM-81 Created SQL tables and connections for the user functionality

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -1,11 +1,11 @@
 CREATE SCHEMA IF NOT EXISTS `ims`;
 
 USE `ims` ;
-
 CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `id` INT(11) NOT NULL AUTO_INCREMENT,
     `first_name` VARCHAR(40) DEFAULT NULL,
     `surname` VARCHAR(40) DEFAULT NULL,
+    `UserID` int(11) not null unique,
     PRIMARY KEY (`id`)
 );
 CREATE TABLE IF NOT EXISTS `ims`.`items` (
@@ -22,4 +22,25 @@ CREATE TABLE IF NOT EXISTS `ims`.`orders` (
 Create table if not exists `ims`.`orderItems`(
    `ItemID` INT(11) NOT NULL,
    `OrderID` INT(11) NOT NULL
+);
+Create table if not exists `ims`.`Users`(
+ `id` INT(11) NOT NULL AUTO_INCREMENT,
+ `Username` VARCHAR(40) DEFAULT NULL,
+ `Password` VARCHAR(40) DEFAULT NULL,
+ `permissions` int default 0
+);
+Create table if not exists `ims`.`ItemEdits`(
+`EditorID` INT(11) not null,
+`ItemID` INT(11) not null,
+`ChangeType` char(6) default null
+);
+Create table if not exists `ims`.`OrderEdits`(
+`EditorID` INT(11) not null,
+`OrderID` INT(11) not null,
+`ChangeType` char(6) default null
+);
+Create table if not exists `ims`.`CustomerEdits`(
+`EditorID` INT(11) not null,
+`CustomerID` INT(11) not null,
+`ChangeType` char(6) default null
 );
